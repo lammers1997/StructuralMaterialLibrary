f2c174f36efe98b318db40b35c28fa682f8438c1
Timo Lamminen <timo.lamminen@tuni.fi>
2024-04-06 14:50:54 +0300

    push all

--- frontend/StructMatLibFront/src/pages/Home.jsx
+++ frontend/StructMatLibFront/src/pages/Home.jsx
@@ -1,8 +1,9 @@
 const Home = () => {
     return (
         <>
-            <h2>Home</h2>
-            <p>This page provides a complete material library for all materials needed in structural engineering</p>
+            <h2>About this website:</h2>
+            <p>This page provides a complete material library for materials needed in structural engineering</p>
+            <p>Each material has information on its properties</p>
         </>
     )
 }
